49282806ef3d56566d143df942a8e105f93d0dd8
AnnisaFitry <2041720123@student.polinema.ac.id>
2022-03-21 11:13:44 +0700

    first()

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -10,8 +10,9 @@ class PegawaiController extends Controller
     public function index()
     {
     	// mengambil data pegawai
-    	$pegawai = Pegawai::all();
- 
+    	//$pegawai = Pegawai::all();
+		// mengambil data pegawai yang pertama
+		$pegawai = Pegawai::first(); 
     	// mengirim data pegawai ke view pegawai
     	return view('pegawai', ['pegawai' => $pegawai]);
     }
